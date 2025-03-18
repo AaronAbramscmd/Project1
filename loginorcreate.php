8c31c620c928414f8ec320d4aa711345598b6c8c
AaronAbramscmd <abrams433@morrisville.edu>
2025-03-18 11:48:11 -0400

    Initial commit

---
+++ loginorcreate.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	
+	<title></title>
+</head>
+<body>
+	<form>
+		<p>Email: <input type="text" name="email"></p>
+		<p>Password: <input type="text" name="password"></p>
+		<input type="submit" value="Login" name="name"> or 
+		<input type="submit" value="Create an account" name="account">
+	</form>
+
+</body>
+</html>
